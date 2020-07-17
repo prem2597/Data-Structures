99388eddd02bf236a755bfbc656e4637561d96c6
Prem Kumar <prembammidi25@msitprogram.net>
2020-07-17 22:43:48 +0530

    Binary search logic

--- Binary Search/BinarySearch.java
+++ Binary Search/BinarySearch.java
@@ -1,10 +1,31 @@
 import java.util.Arrays;
 /**
- * Stack
+ * Binary Search
  * @author Prem
  */
  public class BinarySearch {
+
+   public static int indexOf(int[] arr, int key) {
+     int low  = 0;
+     int high = arr.length - 1;
+     while (low <= high) {
+       int mid = low + (high - low)/2;
+       if (key < arr[mid]) {
+         high = mid - 1;
+       } else if (key > arr[mid]) {
+         low = mid + 1;
+       } else {
+         return mid;
+       }
+     }
+     return -1;
+   }
+
+   public static int rank(int[] arr, int key) {
+     return indexOf(arr, key);
+   }
+
    public static void main(String[] args) {
-     
+     //
    }
  }
